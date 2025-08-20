24e7835ded7e16bb7848ba12c33ea2c1036fa448
Sukhpreet Singh <sukhpreetsingh12323@gmail.com>
2025-08-20 10:26:43 +0530

    admin route defined

--- admin/routes.py
+++ admin/routes.py
@@ -1,5 +1,5 @@
 # admin/routes.py
-from flask import render_template, abort, redirect, request
+from flask import render_template, abort, redirect, request, url_for
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from extensions import db
 from auth.models import User
@@ -8,7 +8,7 @@ from . import admin_bp
 def _guard_or_redirect():
     uid = get_jwt_identity()
     if not uid:
-        return redirect(f"/auth/signin?next={request.path}")
+        return redirect(url_for('index'))
     user = db.session.get(User, int(uid))
     # use your hybrid props from the auth model
     if not user or not (user.is_admin_user or user.is_master_user):
